2b9adbe8002feac40931e32323dfc5dbe37238cd
Feras <feras.altalib@gmail.com>
2025-07-03 19:20:22 +0200

    Update API_DOMAIN in config.js to point to the production server

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 // Configuration file - Change this domain to match your API
 const CONFIG = {
-    API_DOMAIN: 'http://localhost:8000', // Change this to your actual domain
+    // API_DOMAIN: 'https://localhost:8000', 
+    API_DOMAIN: 'http://193.134.250.16',
     API_ENDPOINTS: {
         HOME: '/api/home',
         REGISTER: '/api/register',
